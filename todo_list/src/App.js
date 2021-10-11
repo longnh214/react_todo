74dd2b4c3ffdfdaea63e5b740e1a40e7a2d43acc
naknak <cnh6123@naver.com>
2021-10-11 18:01:40 +0900

    [update] Form Component 추가

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import TodoListTemplate from "./components/TodoList/TodoListTemplate";
+import Form from "./components/Form/Form";
 
 class App extends Component {
     render() {
-        return <TodoListTemplate>React 프로젝트 만들기!</TodoListTemplate>;
+        return (
+            <TodoListTemplate form={<Form />}>
+                React 프로젝트 만들기!
+            </TodoListTemplate>
+        );
     }
 }
 
